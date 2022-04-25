96331fd4fcda5860d32403cfbf8250c548ffba01
Robson Silva <robsantossilva@gmail.com>
2022-04-25 19:49:20 -0300

    product usecase

---
+++ src/usecase/product/create/create.product.unit.spec.ts
@@ -0,0 +1,58 @@
+import { InputCreateProductDto } from "./create.product.dto";
+import CreateProductUseCase from "./create.product.usecase";
+
+const input: InputCreateProductDto = {
+    name: "Product 1",
+    price: 10
+};
+
+const MockRepository = () => {
+    return {
+        find: jest.fn(),
+        findAll: jest.fn(),
+        create: jest.fn(),
+        update: jest.fn(),
+    };
+};
+
+describe("Unit test create product use case", () => {
+
+    it("should create a product", async () => {
+
+        const productRepository = MockRepository();
+        const createProductUseCase = new CreateProductUseCase(productRepository);
+
+        const output = await createProductUseCase.execute(input);
+
+        expect(output).toEqual({
+            id: expect.any(String),
+            name: input.name,
+            price: input.price
+        });
+
+    });
+
+    it("should thrown an error when name is missing", async () => {
+        const productRepository = MockRepository();
+        const createProductUseCase = new CreateProductUseCase(productRepository);
+
+        input.name = "";
+
+        await expect(createProductUseCase.execute(input)).rejects.toThrow(
+            "Name is required"
+        );
+    });
+
+    it("should thrown an error when street is missing", async () => {
+        const productRepository = MockRepository();
+        const createProductUseCase = new CreateProductUseCase(productRepository);
+
+        input.name = "Product Name";
+        input.price = -1;
+
+        await expect(createProductUseCase.execute(input)).rejects.toThrow(
+            "Price must be greater than zero"
+        );
+    });
+
+});
